1bb90c4566218105bf0694b5e6fbaa2368cecc79
Caleb Madrigal <cmadrigal>
2013-08-18 22:39:25 -0500

    Added area under a curve example.

--- monte_carlo_pi.py
+++ monte_carlo_pi.py
@@ -3,14 +3,14 @@ import random
 NUM_POINTS = 10000
 
 # Generates random numbers between -1 and 1
-def rand(): return random.random() * 2 - 1
+def rand(): return random.uniform(-1,1)
 
 # Generate a bunch of random points in the square which inscribes the unit circle.
-all_points = [(rand(), rand()) for i in xrange(NUM_POINTS)]
+points = [(rand(), rand()) for i in xrange(NUM_POINTS)]
 
 # Find all points which are inside the circle - i.e. points which match the formula for
 # a circle: x**2 + y**2 <= 1
-points_in_circle = filter(lambda point: point[0]**2 + point[1]**2 <= 1, all_points)
+points_in_circle = filter(lambda point: point[0]**2 + point[1]**2 <= 1, points)
 
-print "Estimate of pi:", 4.0 * len(points_in_circle) / len(all_points)
+print "Estimate of pi:", 4.0 * len(points_in_circle) / len(points)
 
